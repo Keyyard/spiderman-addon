8d784b4bf50cc108d6efdd6bf18a90c1383285ec
SoraTheDuck <139145953+ArosMailWork@users.noreply.github.com>
2026-06-24 17:49:44 +0700

    C O L O R

--- src/Ults/LoreMapDB.js
+++ src/Ults/LoreMapDB.js
@@ -25,18 +25,35 @@ export function GetLoreById(itemTypeId) {
 }
 
 // --- INITIALIZATION CALLS ---
-// This registers all items from your list using the RegisterLore function.
-RegisterLore("curio:copper_rocket_wand", "§7Right-click to perform a §e Dash/Boosts §bElytra§7.\\n§6⚙ Auto-repairs with Copper Nuggets each 2 seconds");
-RegisterLore("curio:enderbelt", "§7A forgotten belt that contain enderman secret technique§7.\\n§6 Sneak while use ender pearl will instantly teleport player 25 blocks straight");
-RegisterLore("curio:evolve_badget_1", "§7I wonder what the hidden behind this§7.");
-RegisterLore("curio:evolve_badget_2", "§7A relic that contain Relic Power, I wonder what Relics actually for §7.");
-RegisterLore("curio:evolve_badget_3", "§7A new hero ascended, strike down the worst Catastrophe§7.\\n§6+ Immune to fall damage");
-RegisterLore("curio:evolve_badget_4", "§7Through glory and ashes, A Divine path shall revealed§7.\\n§6Ahero fallen, cursed by evil");
-RegisterLore("curio:made_in_heaven", "§7Peace for humanity§7.");
-RegisterLore("curio:invisicloak", "§7Batman prototype item§7.\\n§6Invisible while in the dark");
-RegisterLore("curio:killer_queen", "§7To keep an ordinary life, we need to delete the problem in silent§7.\\n§6Every projectile now turn to a silent bomb, destroy items or blocks");
-RegisterLore("curio:killer_queen_btd", "§7Killer Queen third bomb§7.\\n§6Ignore the Death and wake up at comfy home\\n§6Sneak + Right Click to place the time anchor");
-RegisterLore("curio:ragewatch", "§7We dont have that much time, dont block my way§7.\\n§6Player when get hitted will gain Strength effect for 5 secs, stackable");
-RegisterLore("curio:reverse_watch", "§7A watch that contain space and time ? too OP §7.\\n§6 Keep on hotbar, Right Click to Reverse the time");
-RegisterLore("curio:spider_bracelet", "§7Spooder man§7.\\n§6Right click to swing\\n§6Hold it in curios inventory to get Spider Power !!!!");
-RegisterLore("curio:torchplacer", "§7Scare of the dark ? dont worry, Torch placer 3000 is here§7.\\n§6⚙ have torch on ur hotbar and bye bye darkness");
+
+// Copper Rocket Wand
+RegisterLore("curio:copper_rocket_wand", "§7§eRight-click§7 to perform a §eDash/Boosts Elytra§7.\\n§a⚙ Auto-repairs§7 with §6Copper Nuggets§7 each 2 seconds");
+
+// Enderbelt
+RegisterLore("curio:enderbelt", "§7A forgotten belt that contain §l§fenderman secret technique§7.\\n§eSneak§7 while use §6ender pearl§7 will §ainstantly teleport§7 player 25 blocks");
+
+// Evolve Badgets
+RegisterLore("curio:evolve_badget_1", "§7I wonder what is §l§fhidden§7 behind this...");
+RegisterLore("curio:evolve_badget_2", "§7A relic that contain §l§fRelic Power§7, I wonder what Relics are for.");
+RegisterLore("curio:evolve_badget_3", "§7A new hero ascended, strike down the §l§fworst Catastrophe§7.\\n§a+ Immune to fall damage");
+RegisterLore("curio:evolve_badget_4", "§7Through §l§fglory and ashes§7, A Divine path shall be revealed...\\n§c§lA hero fallen, cursed by evil");
+
+// Made in Heaven
+RegisterLore("curio:made_in_heaven", "§7Peace for §l§fhumanity§7.");
+
+// Invisicloak
+RegisterLore("curio:invisicloak", "§7Batman prototype item.\\n§aInvisible§7 while in the dark");
+
+// Killer Queen Items
+RegisterLore("curio:killer_queen", "§7To keep an §l§fordinary life§7, we need to §l§fdelete the problem§7 in silent.\\n§aEvery projectile§7 now turns to a §asilent bomb§7, §adestroying items or blocks");
+RegisterLore("curio:killer_queen_btd", "§7Return by §l§fDeath§7, back to ordinary life.\\n§eSneak + Right Click§7 to §aplace time anchor");
+
+// Watches
+RegisterLore("curio:ragewatch", "§7We don't have that much time, §l§fdont block my way§7.\\n§7When hit, §again Strength effect§7 for 5 secs (Stackable)");
+RegisterLore("curio:reverse_watch", "§7A watch that contains §l§fspace and time§7? Too OP.\\n§7Keep on hotbar, §eRight Click§7 to §aReverse the time");
+
+// Spider Bracelet
+RegisterLore("curio:spider_bracelet", "§7§l§fSpooder man§7.\\n§eRight click§7 to §aswing§7.\\n§7Hold in curios inventory to §aget Spider Power !!!!");
+
+// Torch Placer
+RegisterLore("curio:torchplacer", "§7Scared of the dark? Don't worry, Torch Placer 3000 is here.\\n§a⚙ Fuel§7 with §6torches§7 on hotbar and §abye bye darkness");
