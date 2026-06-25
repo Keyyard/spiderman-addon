84144148e91cc0892aed07338fadfd7633d84c0c
Beyond64 <beyondongoing@gmail.com>
2026-06-25 22:19:03 +0600

    +Mimic summon and mimic kill

---
+++ src/Mimic/Summon.js
@@ -0,0 +1,67 @@
+import { BlockComponentTypes, system } from "@minecraft/server";
+
+const RELICS = [
+  "curio:made_in_heaven",
+  "curio:copper_rocket_wand",
+  "curio:enderbelt",
+  "curio:evolve_badget_1",
+  "curio:evolve_badget_2",
+  "curio:evolve_badget_3",
+  "curio:evolve_badget_4",
+  "curio:invisicloak",
+  "curio:killer_queen_btd",
+  "curio:killer_queen",
+  "curio:ragewatch",
+  "curio:reverse_watch",
+  "curio:spider_bracelet",
+  "curio:torchplacer",
+];
+
+const REQUIRED_ITEM = "minecraft:apple";
+
+export class SummonMimic {
+  static onInteract(player, block, itemStack) {
+    if (!itemStack || itemStack.typeId !== REQUIRED_ITEM) return;
+
+    const inv = block.getComponent(BlockComponentTypes.Inventory);
+    if (!inv?.container) return;
+
+    const container = inv.container;
+
+    let hasRelic = 0;
+
+    for (let i = 0; i < container.size; i++) {
+      const item = container.getItem(i);
+
+      if (item && RELICS.includes(item.typeId)) {
+        hasRelic++;
+      }
+    }
+
+    if (hasRelic == 0) return;
+
+    const entity = player.dimension.spawnEntity(
+      "artifacts:mimic",
+      block.location,
+    );
+    entity.setDynamicProperty("hasRelic", hasRelic);
+
+    block.setType("minecraft:air");
+
+    system.runTimeout(() => {
+      for (const entity of player.dimension.getEntities({
+        location: block.location,
+        maxDistance: 4,
+      })) {
+        const itemComp = entity.getComponent("minecraft:item");
+        if (!itemComp) continue;
+
+        const typeId = itemComp.itemStack?.typeId;
+
+        if (typeId && RELICS.includes(typeId)) {
+          entity.remove();
+        }
+      }
+    }, 1);
+  }
+}
